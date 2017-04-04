2f67376c272473f69d0dbeaa3e4f67024c8d5578
Jevinalys <jevinalyskhamasi@gmail.com>
2017-04-04 16:24:42 +0300

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,25 @@
+class Fruits(objects):
+    def __init__(self,fruit):
+        self.fruit = fruit
+        def eatable_fruit(fruit):
+            "defines fruits that are eaten"
+            "return whether eaten or not"
+         def taste(fruit):
+             "taste of the fruit"
+     fruitmango = Fruits(mango):
+         "this creates and object of a fruit,an instance of the fruit"
+         ""
+         taste=fruitmango.taste()
+         """this is encapsulation because the other method in parent class is
+         not accesible until it is called"""
+     fruitlemon = Fruits(lemon)
+     fruitlemon.taste()= "bitter"
+     fruitmango.taste()= "sweet"
+     """this illustrates polymophism both fruits inherit the same method from parent
+     but each fruit has different taste behaviour"""
+
+     class wildfruits(Fruits):
+         "this shows inheritance all wild fruits are fruits"
+         "hence inherits all methods of parent class fruit"
+
+         
