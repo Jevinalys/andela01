4173a7be4687d3a527172f2046c3f4b6cec985db
Jevinalys <jevinalyskhamasi@gmail.com>
2017-04-05 17:20:34 +0300

    Printing out prime numbers

---
+++ primenum.py
@@ -0,0 +1,14 @@
+
+lower = 0
+upper = 1000000
+
+print("Prime numbers")
+
+for num in range(lower,upper + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
